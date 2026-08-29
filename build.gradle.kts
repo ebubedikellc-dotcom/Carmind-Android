e520d896b727ff67e6e5c8e239cbcad64b8949ff
ebubedikellc-dotcom <ebubedikellc@gmail.com>
2026-08-29 22:02:42 +0100

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "online.carmindai.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "online.carmindai.app"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 3
-        versionName = "3.0.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions { jvmTarget = "17" }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-ktx:1.10.1")
-    testImplementation("junit:junit:4.13.2")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
